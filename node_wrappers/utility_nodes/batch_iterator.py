6c04847db51e7f2e1d373867f1d9dee809ef0a81
RyanOnTheInside <7623207+ryanontheinside@users.noreply.github.com>
2025-04-21 13:33:13 -0400

    consiolidate batch utils

--- node_wrappers/utility_nodes/batch_iterator.py
+++ node_wrappers/utility_nodes/batch_iterator.py
@@ -79,11 +79,37 @@ class BatchIterator:
         
         return (current_image,)
 
-# Mappings for __init__.py
+class BatchInfo:
+    """Node that extracts size and count information from an image batch."""
+    CATEGORY = "Livepeer/Utils"
+    RETURN_TYPES = ("IMAGE", "INT", "INT", "INT")
+    RETURN_NAMES = ("images", "height", "width", "count")
+    FUNCTION = "get_batch_info"
+    
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "images": ("IMAGE",),
+            }
+        }
+
+    def get_batch_info(self, images):
+        # Get dimensions from the image tensor
+        # ComfyUI images are in BHWC format (Batch, Height, Width, Channels)
+        batch_size = images.shape[0]
+        height = images.shape[1]
+        width = images.shape[2]
+        
+        # Return original images and the extracted dimensions
+        return (images, height, width, batch_size)
+    
 NODE_CLASS_MAPPINGS = {
     "BatchIterator": BatchIterator,
+    "BatchInfo": BatchInfo,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "BatchIterator": "Batch Image Iterator",
-} 
+    "BatchInfo": "Batch Info",
+}
